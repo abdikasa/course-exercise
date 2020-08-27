269a6d5bf9a78a891db9f76df1f0462846c971b6
abdikasa <abdikasa@sheridancollege.ca>
2020-08-27 17:19:30 -0400

    Starting exercise 2.2

--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// 2.1: Course information step6 --- FINSIHED
+// Define a component responsible for formatting a single course called Course.
+// Hence, the Course component contains the components defined in the previous part, which are responsible for rendering the course name and its parts.
+
+// 2.2: Course information step7 ----STARTING
+// Show also the sum of the exercises of the course.
+
 ReactDOM.render(<App />, document.getElementById("root"));
