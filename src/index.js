2ea94f805809f53f7636df544713d722438db7ff
abdikasa <abdikasa@sheridancollege.ca>
2020-08-27 15:23:23 -0400

    Starting to refactor modules.

--- src/index.js
+++ src/index.js
@@ -22,15 +22,15 @@ const notes = [
   },
 ];
 
-const App = (props) => {
-  const { notes } = props;
+const App = ({ notes }) => {
+  //const { notes } = props;
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
       </ul>
     </div>
   );
