42ee3e720755871e6c1d46278f16955eecd7fca5
abdikasa <abdikasa@sheridancollege.ca>
2020-08-28 12:05:52 -0400

    Updated the task list needed to complete the exercise.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,16 @@ import App from "./App";
 // Define a component responsible for formatting a single course called Course.
 // Hence, the Course component contains the components defined in the previous part, which are responsible for rendering the course name and its parts.
 
-// 2.2: Course information step7 ----STARTING
+// 2.2: Course information step7 ----FINISHED
 // Show also the sum of the exercises of the course.
+
+// 2.3*: Course information step8 ----FINISHED
+// If you haven't done so already, calculate the sum of exercises with the array method reduce.
+
+// 2.4: Course information step9  ----FINISHED
+// Let's extend our application to allow for an arbitrary number of courses
+
+// 2.5: separate module ----FINISHED
+// Declare the Course component as a separate module, which is imported by the App component. You can include all subcomponents of the course into the same module.
 
 ReactDOM.render(<App />, document.getElementById("root"));
